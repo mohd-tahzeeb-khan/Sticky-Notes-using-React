29fe4f6593751f3bc9c0962a5cd48084a81d8636
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-12-11 11:33:03 +0530

    Minor changes in Cards file

--- Todo/src/components/Card.jsx
+++ Todo/src/components/Card.jsx
@@ -5,14 +5,14 @@ import { IoIosCloseCircle } from "react-icons/io";
 import { motion } from "framer-motion"
 function Card({data, reference}) {
   return (
-    <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2} className='relative w-60 p-5 h-80 flex-shrink-0 bg-zinc-900 rounded-[20px] overflow-hidden'>
-        <IoDocumentTextSharp size='1rem' color='#fff' />
+    <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2} className='relative w-60 p-5 h-80 flex-shrink-0 bg-gray-100 rounded-[20px] overflow-hidden'>
+        <IoDocumentTextSharp size='1rem' color='#000' />
         <p className='text-sm leading-tight mt-5 font-semibold'>{data.description}</p>
         <div className='absolute footer bottom-0  w-full h-10 left-0'>
             <div className='px-5 flex justify-between mb-3'>
                 <h5>{data.filesize}</h5>
-                <span className='w-7 h-7 bg-sky-200 rounded-full flex items-center justify-center'>
-                    {data.close ? <IoIosCloseCircle />:<MdFileDownload size="1rem" color='#fff' />}
+                <span className='w-7 h-7 bg-red-100 rounded-full flex items-center justify-center'>
+                    {data.close ? <IoIosCloseCircle />:<MdFileDownload size="1rem" color='#000' />}
                     
                 </span>
             </div>
