bcbb99e06023f84642b9caaa995f81bafd9ae80f
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-12-11 11:32:29 +0530

    Minor changes in Background file

--- Todo/src/components/Background.jsx
+++ Todo/src/components/Background.jsx
@@ -3,8 +3,8 @@ function Background() {
   return (
     <>
       <div className='fixed z-[2] w-full h-screen'>
-          <div className='w-full py-10 flex justify-center text-zinc-900 font-semibold text-xl absolute top-[5%]'>Documents</div>
-          <h1  className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] leading-none tracking-tighter text-red-200'>TODO</h1>
+          <div className='w-full py-10 flex justify-center text-zinc-900 font-semibold text-xl absolute top-[5%]'>Stick Your Notes</div>
+          <h1  className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] leading-none tracking-tighter text-red-200'>NOTES.</h1>
       </div>
     </>
     
